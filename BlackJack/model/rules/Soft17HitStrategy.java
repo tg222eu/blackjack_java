00008b7fd4dce4fde7de5b63e18a6ccc7a52a490
Mauro <mauropappaterra@gmail.com>
2015-10-20 20:36:49 +0200

    Rejected commit: Default

---
+++ BlackJack/model/rules/Soft17HitStrategy.java
@@ -0,0 +1,23 @@
+package BlackJack.model.rules;
+
+import BlackJack.model.Player;
+
+/**
+ * Created by Mauro José Pappaterra on 20/10/2015.
+ */
+
+public class Soft17HitStrategy {
+    private final int g_hitLimit = 17;
+
+    public boolean DoHit(Player a_dealer) {
+
+        if (a_dealer.CalcScore() < g_hitLimit){
+            return true;
+        }
+
+        if (a_dealer.CalcScore() > g_hitLimit && a_dealer.AceOnHand()){ // if dealer has more than 17 point and one Ace in his hand
+            return true; }
+
+    return false;
+    }
+}
